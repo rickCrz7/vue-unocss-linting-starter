6b09c874c10e8be39d2bbb7d459ddc20c2b70ccd
ricardocruz <ricardo.cruz@swau.edu>
2025-02-15 20:15:22 -0600

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+// src/main.ts
+import '@unocss/reset/tailwind.css';
+import 'virtual:uno.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.css';
+
+createApp(App).mount('#app');
